c57995def6983de8ad8152f17edf0d5c32baab9a
KyleW <Kyle.Warneck@gmail.com>
2014-01-15 14:15:47 -0800

    mocha is up and running, but still working on initial tests and coverage reports

--- test/test.js
+++ test/test.js
@@ -1,5 +1,11 @@
+
 var assert = require("chai").assert;
-// var assert = require("assert");
+var expect = require('chai').expect;
+var jsc = require('jscoverage');
+
+
+
+// Default to make sure mocha is working
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
@@ -7,4 +13,45 @@ describe('Array', function(){
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+});
+
+
+// Server Testing
+
+
+describe('The server', function(){
+
+  var server = require('../server/server.js');
+
+  describe('starts', function(){
+    
+    it('and has run all the startup stuff', function(){
+      expect(server.app).to.be.ok;
+    });
+
+    it('Responds to a GET request at /', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+
+
+});
+
+
+// Client Testing
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+
+// Coverage Testing
+process.on('exit', function () {
+  jsc.coverage(); // print summary info, cover percent
+  jsc.coverageDetail(); // print uncovered lines
 });
