ea3066bd1bb505199ebc760c8b9b9595c0b869a1
KyleW <Kyle.Warneck@gmail.com>
2014-01-16 09:32:37 -0800

    angular setup is almost there

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,11 @@
 angular.module('app', ['ngTouch'])
 
 .config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/', {
+  $routeProvider
+  .when('/', {
     templateUrl: '../templates/example.html'
-  }).when('/404', {
+  })
+  .when('/404', {
     templateUrl: '../templates/404.html'
   })
   .otherwise({redirectTo: '/404'});
