b65fcba83a8502a87b75ae18dac98a8837daa340
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-15 20:26:53 -0800

    minor fix for api config in the external file

--- server/middleware/callyummly.js
+++ server/middleware/callyummly.js
@@ -1,16 +1,18 @@
 var http = require('http')
-var apiauth = require('../configfile.js');
+var config = require('../configfile.js');
 
 exports.yumoptions = yumoptions = {
   host: 'http://api.yummly.com/v1'
 }
+
+var apiauth = config.yumapi;
 
 exports.searchRecipe = searchRecipe = function(searchTerms, callback) {    
 // The base url for the Search Recipes GET is
 // http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your_search_parameters
   var options = yumoptions;
   options.path = '/api/recipes'+ apiauth.token + '&' + searchTerms;
-  console.log( );
+  console.log( apiauth );
 
   http.get( 
     options.host+options.path, 
