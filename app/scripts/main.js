b76e30d20f1a807f011b25917fceb213e7fb1b9d
KyleW <Kyle.Warneck@gmail.com>
2014-01-15 20:37:25 -0800

    implemented angular rounting, but needs testing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,11 @@
 
-angular.module('app', ['ngTouch']);
+angular.module('app', ['ngTouch'])
+
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: '../templates/example.html'
+  }).when('/404', {
+    templateUrl: '../templates/404.html'
+  })
+  .otherwise({redirectTo: '/404'});
+}]);
