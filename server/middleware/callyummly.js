b2a9e6b4fde5bdc8cfd0fd4a067767da1db04917
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-15 16:33:01 -0800

    minor changes

--- server/middleware/callyummly.js
+++ server/middleware/callyummly.js
@@ -8,10 +8,15 @@ var yumapi = {
 // The base url for the Search Recipes GET is
 // http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your _search_parameters
 var yumoptions = {
-
+  host: 'api.yummly.com/v1',
+  port: 80,
+  path: '/index.html'
 }
 
-var searchRecipe = function(req, options, callback) {
+var searchRecipe = function(req, options, parameters, callback) {
+  
+  
+
   http.get(options, function(res) {
     console.log('Got success ',res)
   })
