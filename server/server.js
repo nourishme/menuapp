8ab0c9a0633d3032ca267c45389c4dbce426b9be
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-16 12:06:23 -0800

    removing commented lines from server.js

--- server/server.js
+++ server/server.js
@@ -23,24 +23,10 @@ app.set('title', 'menuapp');
  * Routes
  */
 
- console.log(routes)
-
 app.use('/', express.static( __dirname+ '/../app'));
 app.use('/db', routes.dbcall );
 app.use('/apisearch', routes.yumSearch );
 app.use('/apiget', routes.yumGet );
-
-
-// app.get('/db', function(req, res){
-//   res.send(neo4jDB.goodbyeNode.data);
-// });
-
-// app.get('/apitest', function(req, res) {
-//   // var result = 
-//   yummly.searchRecipe('kale', function(result){ res.send(result)} );
-//   // res.send(result);
-
-// })
 
 //Start the app by listening on <port>
 var port = 3000;
