35cf1b0955cbfade9ea5c4e09f5c9e78c253bedd
Kamla Kasichainula <kasikamla@gmail.com>
2014-01-18 15:52:07 -0800

    pulling kyle's changes

--- server/neo4jDB.js
+++ server/neo4jDB.js
@@ -4,10 +4,10 @@ exports.db = new exports.neo4j.GraphDatabase('http://localhost:7474');
 
 exports.goodbyeNode = exports.db.createNode({goodbye: "other world"});     // instantaneous, but...
 
-exports.goodbyeNode.save(function (err, goodbyeNode) {    // ...this is what actually persists.
-  if (err) {
-    console.err('Error saving new node to database:', err);
-  } else {
-    console.log('Node saved to database with id:', goodbyeNode.id);
-  }
-});
+// exports.goodbyeNode.save(function (err, goodbyeNode) {    // ...this is what actually persists.
+//   if (err) {
+//     console.log('Error saving new node to database:', err);
+//   } else {
+//     console.log('Node saved to database with id:', goodbyeNode.id);
+//   }
+// });
