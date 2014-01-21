2417e53b65a7102707bf2c0f7765780ee0c76d60
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-21 15:19:46 -0800

    magical db.phrase.template.js will create many phrases for you if you use it right.

--- server/ingredients.js
+++ server/ingredients.js
@@ -3,14 +3,15 @@ var dbinventory = require('./middleware/dbinventory.js');
 
 module.exports = {
   getUsersRecipeList: function(req,res){
-    // var ingredients = dbinventory.getUserInventory(req.userid); 
+    // var ingredients = dbinventory.getUserInventory(req.userid); //TODO: this should work, isntead of hardcode below
     var ingredients = dbinventory.getUserInventory(406842); 
     res.send(ingredients);
   },
 
   saveUsersList: function(req,res){
     console.log(req.body);
-    res.send(201);
+    var update = updateUserInventory(req);
+    res.send(update);
   },
 
   likeIngredientList: function(res, req){
--- server/middleware/dbinventory.js
+++ server/middleware/dbinventory.js
@@ -1,20 +1,42 @@
 var neo4jDB = require('../neo4jDB.js');
 exports.neo4j = neo4j = require('node-neo4j');
 exports.db = db = new neo4j('http://localhost:7474');
+exports.phrases = ph = require('../middleware/db.phrase.templates.js');
 
-exports.updateUserInventory = update = function(userid, invChangeObj, cb) {
-  var statement = createStatementFromObject(invChangeObject);
+var cb = function(err, result) {
+    if (err) throw err;
+    console.log(err, result);
+    return result.data;
+  };
+
+exports.updateUserInventory = update = function(request) {
+  var userid = request.user;
+  invChangeArray = request.data;
+  var statement = ph.updateLikeStatusStatementFromObject(userid, invChangeArray);
   db.beginAndCommitTransaction(statement, cb);
 };
+
+/* invChangeArray looks like this:
+[ { name: 'carrots', liked: true },
+  { name: 'bacon', liked: true },
+  { name: 'beets', liked: false },
+  { name: 'onions', liked: false } ]
+
+CASE n.eyes
+ WHEN 'blue' THEN 1
+ WHEN 'brown' THEN 2
+ ELSE 3
+END
+
+*/
+
+
 
 exports.getUserInventory = getinventory = function(userid) {
   // match (n) where id(n) = {userid} return n
   // match (n)->[:HAS_INVENTORY]-(b) where id(n) = 406842 return b
   msg = "match (n)-[:HAS_INVENTORY]->(i) where id(n) = "+userid+" return i";
-  var cb = function(err, result) {
-    console.log(err, result);
-    return result;
-  };
+  
   db.cypherQuery(msg, cb);
   
 };
