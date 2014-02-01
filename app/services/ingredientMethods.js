950a051db74ebf77aaae9c49926a26ab07602c71
KyleW <Kyle.Warneck@gmail.com>
2014-02-01 10:01:03 -0800

    add filter on suggested ingredients back in

--- app/services/ingredientMethods.js
+++ app/services/ingredientMethods.js
@@ -35,10 +35,10 @@ app.factory('ingredientMethods', function ($http,sharedProperties) {
           console.log("filtered list ", data.filter(function(ingredient){
             return currentList.indexOf(ingredient._id) === -1;
           }));
-          $scope.suggestedIngredients = data;
-          // $scope.suggestedIngredients = data.filter(function(ingredient){
-          //   return currentList.indexOf(ingredient._id) === -1;
-          // });
+          // $scope.suggestedIngredients = data;
+          $scope.suggestedIngredients = data.filter(function(ingredient){
+            return currentList.indexOf(ingredient._id) === -1;
+          });
         })
         .error(function(data, status){
           console.log(data,status);
