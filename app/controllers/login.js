0d01e0b0011b0d4115429a6fb352bca5993feb5f
KyleW <Kyle.Warneck@gmail.com>
2014-02-08 16:55:39 -0800

    added header to login page

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,4 @@
 
 app.controller('login',  ['$scope', function($scope) {
-  document.getElementById("headercontainer").style.display="none";
+  // document.getElementById("headercontainer").style.display="none";
 }]);
